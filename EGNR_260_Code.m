12d18e61bc15ee3c0b4882a1c3615fa53d10d90d
Josh Schember <joshuaschember@gmail.com>
2020-02-21 15:24:19 -0500

    Add files via upload
    Initial Upload

---
+++ EGNR_260_Code.m
@@ -0,0 +1,15 @@
+
+%{
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*
+*   EGNR260 2019-2020 Research Methods
+*
+*   Title:      T-20 Time Calculator
+*   Authors:    Cameron Cook, Lauren Niemiec, Joshua Schember
+*               Veronica Shull, Sabrina VanAuker
+*   Created:    Feb. 21, 2020
+*   Modified:   Feb. 21, 2020
+*   Function:   
+*
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+%}
